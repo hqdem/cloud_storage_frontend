e476d0e3e86ed36d85f53f598935a28a58339729
hqdem <ilyadem4@gmail.com>
2023-03-10 17:56:13 +0300

    add base footer styles

--- src/layouts/Footer/footer.jsx
+++ src/layouts/Footer/footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import classes from './footer.module.css'
 
 const Footer = () => {
+    const [year, setYear] = useState('')
+
+    useEffect(() => {
+        setYear(String(new Date().getFullYear()))
+    }, [])
+
     return (
         <div className="section">
-            Footer
+            <div className={classes.footer_text}>
+                <span>&copy; {year} HqDev</span>
+            </div>
         </div>
     )
 }
