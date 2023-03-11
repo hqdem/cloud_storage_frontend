de3dabebbd3a957938a9d1acbed4adce14653c53
hqdem <ilyadem4@gmail.com>
2023-03-11 15:24:12 +0300

    add owner label to the file component

--- src/components/Files/FileItem/FileItem.jsx
+++ src/components/Files/FileItem/FileItem.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import classes from './file_item.module.css'
 import File2FillIcon from "remixicon-react/File2FillIcon"
 
-const FileItem = ({name}) => {
+const FileItem = ({name, owner}) => {
     return (
         <div className={classes.file_item}>
             <File2FillIcon />
             <span>{name}</span>
+            <span className={classes.file_owner_label}>{owner}</span>
         </div>
     )
 }
--- src/components/Files/Files.jsx
+++ src/components/Files/Files.jsx
@@ -10,13 +10,13 @@ const Files = () => {
             </div>
 
             <div className={classes.files_items}>
-                <FileItem key={1} name={'Файл'} />
-                <FileItem key={2} name={'Файл'} />
-                <FileItem key={3} name={'Файл'} />
-                <FileItem key={4} name={'Файл'} />
-                <FileItem key={5} name={'Файл'} />
-                <FileItem key={6} name={'Файл'} />
-                <FileItem key={7} name={'Файл'} />
+                <FileItem key={1} name={'Файл'} owner={'Пользователь'}/>
+                <FileItem key={2} name={'Файл'} owner={'Пользователь'}/>
+                <FileItem key={3} name={'Файл'} owner={'Пользователь'}/>
+                <FileItem key={4} name={'Файл'} owner={'Пользователь'}/>
+                <FileItem key={5} name={'Файл'} owner={'Пользователь'}/>
+                <FileItem key={6} name={'Файл'} owner={'Пользователь'}/>
+                <FileItem key={7} name={'Файл'} owner={'Пользователь'}/>
             </div>
         </div>
     )
