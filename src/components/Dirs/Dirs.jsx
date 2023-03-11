91c95546eeeb2dd5b258be41ad90f184b2099b14
hqdem <ilyadem4@gmail.com>
2023-03-11 12:08:52 +0300

    fix styles for growing content

--- src/components/Dirs/Dirs.jsx
+++ src/components/Dirs/Dirs.jsx
@@ -3,20 +3,26 @@ import DirItem from "./DirItem/DirItem.jsx"
 import classes from './dirs.module.css'
 
 const Dirs = () => {
-    return (
-        <div>
+    return (<div className={classes.dirs_section}>
             <div className={classes.dirs_header}>
                 <span>Папки</span>
             </div>
+
             <div className={classes.dirs_items}>
                 <DirItem key={1} name={'Папка'}/>
                 <DirItem key={2} name={'Папка'}/>
                 <DirItem key={3} name={'Папка'}/>
                 <DirItem key={4} name={'Папка'}/>
                 <DirItem key={5} name={'Папка'}/>
+                <DirItem key={5} name={'Папка'}/>
+                <DirItem key={5} name={'Папка'}/>
+                <DirItem key={5} name={'Папка'}/>
+                <DirItem key={5} name={'Папка'}/>
+                <DirItem key={5} name={'Папка'}/>
+                <DirItem key={5} name={'Папка'}/>
             </div>
-        </div>
-    )
+
+        </div>)
 }
 
 export default Dirs
