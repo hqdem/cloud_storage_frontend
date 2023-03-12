422f52c3d453a81fd41b9043d3c69e79a6b6c127
hqdem <ilyadem4@gmail.com>
2023-03-12 11:10:55 +0300

    refactor components location 'cause routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import './App.css'
 import Header from "./layouts/Header/Header.jsx"
 import Main from "./layouts/Main/Main.jsx"
 import Footer from "./layouts/Footer/Footer.jsx"
-import Dirs from "./components/Dirs/Dirs.jsx"
-import Files from "./components/Files/Files.jsx"
+import {Routes, Route} from "react-router-dom"
+import DirsFilesMain from "./components/DirsFilesMain/DirsFilesMain.jsx"
 
 function App() {
-  return (
-    <div className='d_flex-col h-100'>
-        <Header />
-        <Main>
-            <Dirs />
-            <Files />
-        </Main>
-        <Footer />
-    </div>
-  )
+    return (
+        <div className="d_flex-col h-100">
+            <Header/>
+            <Main>
+                <Routes>
+                    <Route path='/' element={<DirsFilesMain />} />
+                </Routes>
+            </Main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/DirsFilesMain/DirsFilesMain.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dirs from "./Dirs/Dirs.jsx"
+import Files from "./Files/Files.jsx"
+
+const DirsFilesMain = () => {
+    return (
+        <>
+            <Dirs />
+            <Files />
+        </>
+    )
+}
+
+export default DirsFilesMain
