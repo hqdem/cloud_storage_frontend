85a801838e54ce6537ff64eb9203efbc88ee61d7
hqdem <ilyadem4@gmail.com>
2023-03-13 19:48:25 +0300

    add styles to the delete button to the File and Dir components

--- src/components/Dirs/DirItem/DirItem.jsx
+++ src/components/Dirs/DirItem/DirItem.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import FolderFillIcon from "remixicon-react/FolderFillIcon"
+import DeleteBin2FillIcon from "remixicon-react/DeleteBin2FillIcon"
 import classes from './dir_item.module.css'
 import {Link} from "react-router-dom"
 
 const DirItem = ({id, name, owner}) => {
     return (
         <Link to={`/dir/${id}`}>
-            <div className={classes.dir_item}>
-                <FolderFillIcon/>
-                <span>{name}</span>
-                <span className={classes.dir_owner_label}>{owner}</span>
+            <div className={classes.dir_item_wrapper}>
+                <div className={classes.dir_item}>
+                    <FolderFillIcon />
+                    <span>{name}</span>
+                    <span className={classes.dir_owner_label}>{owner}</span>
+                </div>
+                <div className={classes.dir_delete_btn}>
+                    <DeleteBin2FillIcon />
+                </div>
             </div>
         </Link>
     )
--- src/components/Files/FileItem/FileItem.jsx
+++ src/components/Files/FileItem/FileItem.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import classes from './file_item.module.css'
 import File2FillIcon from "remixicon-react/File2FillIcon"
+import DeleteBin2FillIcon from "remixicon-react/DeleteBin2FillIcon"
 
 const FileItem = ({name, owner, file}) => {
     return (
-        <a href={file} download>
-            <div className={classes.file_item}>
-                <File2FillIcon/>
-                <span>{name}</span>
-                <span className={classes.file_owner_label}>{owner}</span>
+        <div className={classes.file_item_wrapper}>
+            <a href={file} download>
+                <div className={classes.file_item}>
+                    <File2FillIcon/>
+                    <span>{name}</span>
+                    <span className={classes.file_owner_label}>{owner}</span>
+                </div>
+            </a>
+            <div className={classes.file_delete_btn}>
+                <DeleteBin2FillIcon />
             </div>
-        </a>
+        </div>
     )
 }
 
