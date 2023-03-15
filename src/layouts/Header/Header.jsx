0e5f9c187ed29b18af95c5ab860af081f84dd02a
hqdem <ilyadem4@gmail.com>
2023-03-15 14:17:32 +0300

    add link to the main page in header title

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import classes from './header.module.css'
 import logo from '../../assets/images/logo.png'
 import Search2LineIcon from 'remixicon-react/Search2LineIcon'
+import {Link} from "react-router-dom"
 
 const Header = () => {
     return (
         <div className={"section d_flex-row " + classes.header}>
             <div className={classes.logo}>
                 <img src={logo} alt="logo"/>
-                <span>Диск</span>
+                <Link to={'/'}><span>Диск</span></Link>
             </div>
 
             <div className={classes.search}>
